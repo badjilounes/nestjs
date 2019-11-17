d7d5c59e398cffef94f83cfae83dc52bbe85aade
Badji Lounès <badjilounes@MacBook-Pro-de-Badji.local>
2019-11-17 02:57:41 +0100

    Retrait du dernier build

--- src/auth/model/jwt.const.ts
+++
@@ -1,5 +0,0 @@
-// Configuration de JWT
-export const jwtConstants = {
-    secret: 'jwtIsAwesome',
-    expiresIn: '10h'
-}
--- src/common/role.enum.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Enum qui définit le Role d'un utilisateur
- * Patient
- * Médecin
- */
-export enum RoleEnum {
-    Patient,
-    Doctor
-}
