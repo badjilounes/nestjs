99d4b8390c7204b4dd48ef5273d07b9f263cc499
Badji Lounès <badjilounes@MacBook-Pro-de-Badji.local>
2019-11-17 02:56:30 +0100

    env files ignored env sh files to test env easily node scripts updated to remove env var

---
+++ env/dev.sh
@@ -0,0 +1,13 @@
+export API_BASE_PATH="api"
+export API_HOST_NAME="localhost"
+export API_PROTOCOL="http"
+export DB_HOST="localhost"
+export DB_NAME="gsb"
+export DB_USERNAME="root"
+export DB_PASSWORD=""
+export DB_PORT=3306
+export SWAGGER_HOST_NAME="localhost:3000"
+export SWAGGER_PROTOCOL="https"
+export WEB_HOST_NAME="localhost:4200"
+export WEB_PROTOCOL="http"
+export NODE_ENV="dev"
